775f2a1cb23c0a8adc89cfe4695f1b651399ba24
makson <myEmail.com>
2026-02-05 15:22:25 +0500

    модалка теперь посередине

--- src/components/Notes/header/contextMenu/contextMenu.tsx
+++ src/components/Notes/header/contextMenu/contextMenu.tsx
@@ -6,11 +6,17 @@ function ContextMenu() {
     const [stateContextMenu, setStateContextMenu] = useState<boolean>(false) 
     const contextMenuRef = useRef<HTMLUListElement>(null) //нужно для нажатия снаружи 
     const tribarRef = useRef<HTMLDivElement>(null)
+    const signOutRef = useRef<HTMLImageElement>(null)
+    const [filtersIsActive, setFiltersIsActive] = useState<boolean>(false) 
+    const [signOutIsActive, setSignOutIsActive] = useState<boolean>(false) 
+    
     function handleOnClickOutside(e: MouseEvent) {
         if(e.target instanceof Node 
             && tribarRef.current !== null
             && !tribarRef.current.contains(e.target) 
             && !contextMenuRef.current?.contains(e.target)
+            && signOutRef.current !== null
+            && !signOutRef.current.contains(e.target) 
         ) {
             setStateContextMenu(false)  
         }
@@ -23,14 +29,23 @@ function ContextMenu() {
     }
     return (
             <>
-                <div ref={tribarRef} className={styles.tribar} onClick={()=>setStateContextMenu((prev)=>!prev)}>
+                <div ref={tribarRef} className={styles.tribar} onClick={()=>{
+                    setStateContextMenu((prev)=>!prev)
+                    setSignOutIsActive(false)
+                    setFiltersIsActive(true)
+                }}>
                     ≡
                 </div>
-                <img className={styles.signOut} src="./src/assets/icons/opened_door.png" alt="sign out" />
+                <img ref={signOutRef} className={styles.signOut} src="./src/assets/icons/opened_door.png" alt="sign out" onClick={()=>{
+                    setStateContextMenu((prev)=>!prev)
+                    setFiltersIsActive(false)
+                    setSignOutIsActive(true)
+                }}/>
             {stateContextMenu && (
             <>
                 <ul className={styles.contextMenu} tabIndex={0} ref={contextMenuRef}>
-                    <NotesFilters />
+                    {filtersIsActive&&<NotesFilters />}
+                    {signOutIsActive&&'привки'}
                 </ul>
                 <div className={styles.overlay}></div>
             </>
